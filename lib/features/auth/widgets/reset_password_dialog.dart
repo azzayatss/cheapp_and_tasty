da5d1b5f80caf8571221ae712c5820f45b9c7bcf
andrii zayats <andrii.zayats@otakoyi.com>
2023-09-07 14:36:33 +0300

    feat: sign up form validation

--- lib/features/auth/widgets/reset_password_dialog.dart
+++ lib/features/auth/widgets/reset_password_dialog.dart
@@ -3,6 +3,7 @@ import 'package:cheapp_and_tasty/extensions/build_context_extension.dart';
 import 'package:cheapp_and_tasty/features/auth/controllers/sign_in_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:form_validator/form_validator.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -13,38 +14,43 @@ class ResetPasswordDialog extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final formKey = useMemoized(GlobalKey<FormState>.new);
     final emailController = useTextEditingController();
     return AlertDialog(
       title: Text(context.tr.forgotPassword),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            context.tr.forgotPasswordDialogContent1,
-          ),
-          const SizedBox(
-            height: AppLayouts.defaultPadding,
-          ),
-          Text(
-            context.tr.forgotPasswordDialogContent2,
-          ),
-          const SizedBox(
-            height: AppLayouts.defaultPadding,
-          ),
-          Text(
-            context.tr.forgotPasswordDialogContent3,
-          ),
-          const SizedBox(
-            height: AppLayouts.defaultPadding,
-          ),
-          TextFormField(
-            controller: emailController,
-            decoration: InputDecoration(
-              hintText: context.tr.emailFormHint,
+      content: Form(
+        key: formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              context.tr.forgotPasswordDialogContent1,
             ),
-            keyboardType: TextInputType.emailAddress,
-          ),
-        ],
+            const SizedBox(
+              height: AppLayouts.defaultPadding,
+            ),
+            Text(
+              context.tr.forgotPasswordDialogContent2,
+            ),
+            const SizedBox(
+              height: AppLayouts.defaultPadding,
+            ),
+            Text(
+              context.tr.forgotPasswordDialogContent3,
+            ),
+            const SizedBox(
+              height: AppLayouts.defaultPadding,
+            ),
+            TextFormField(
+              controller: emailController,
+              decoration: InputDecoration(
+                hintText: context.tr.emailFormHint,
+              ),
+              keyboardType: TextInputType.emailAddress,
+              validator: ValidationBuilder().email().build(),
+            ),
+          ],
+        ),
       ),
       actions: <Widget>[
         Consumer(
--- lib/features/auth/widgets/sign_up_form.dart
+++ lib/features/auth/widgets/sign_up_form.dart
@@ -4,6 +4,7 @@ import 'package:cheapp_and_tasty/features/auth/controllers/sign_in_controller.da
 import 'package:cheapp_and_tasty/features/auth/widgets/google_sign_in_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:form_validator/form_validator.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class SignUpForm extends HookWidget {
@@ -11,6 +12,7 @@ class SignUpForm extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final formKey = useMemoized(GlobalKey<FormState>.new);
     final emailController = useTextEditingController();
     final passwordController = useTextEditingController();
     final obscureText = useState(true);
@@ -20,101 +22,128 @@ class SignUpForm extends HookWidget {
       child: Padding(
         padding: const EdgeInsets.all(AppLayouts.defaultPadding),
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: AppLayouts.spacer / 2,
-              ),
-              Text(
-                context.tr.signUpEmoji,
-                style: const TextStyle(fontSize: AppLayouts.emojiSize),
-              ),
-              const SizedBox(
-                height: AppLayouts.defaultPadding,
-              ),
-              TextFormField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  hintText: context.tr.emailFormHint,
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: AppLayouts.spacer / 2,
                 ),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              const SizedBox(
-                height: AppLayouts.defaultPadding,
-              ),
-              TextFormField(
-                controller: passwordController,
-                obscureText: obscureText.value,
-                decoration: InputDecoration(
-                  hintText: context.tr.passwordFormHint,
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      obscureText.value
-                          ? Icons.visibility_outlined
-                          : Icons.visibility_off_outlined,
-                    ),
-                    onPressed: () => obscureText.value = !obscureText.value,
-                  ),
+                Text(
+                  context.tr.signUpEmoji,
+                  style: const TextStyle(fontSize: AppLayouts.emojiSize),
                 ),
-                keyboardType: TextInputType.visiblePassword,
-              ),
-              Row(
-                children: [
-                  Checkbox(
-                    value: termsAndConditions.value,
-                    onChanged: (_) =>
-                        termsAndConditions.value = !termsAndConditions.value,
+                const SizedBox(
+                  height: AppLayouts.defaultPadding,
+                ),
+                TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    hintText: context.tr.emailFormHint,
                   ),
-                  GestureDetector(
-                    child: Text(
-                      context.tr.termsAndConditionsCheckBoxText,
+                  keyboardType: TextInputType.emailAddress,
+                  validator: ValidationBuilder()
+                      .required()
+                      .email()
+                      .maxLength(50)
+                      .build(),
+                ),
+                const SizedBox(
+                  height: AppLayouts.defaultPadding,
+                ),
+                TextFormField(
+                  controller: passwordController,
+                  obscureText: obscureText.value,
+                  decoration: InputDecoration(
+                    hintText: context.tr.passwordFormHint,
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        obscureText.value
+                            ? Icons.visibility_outlined
+                            : Icons.visibility_off_outlined,
+                      ),
+                      onPressed: () => obscureText.value = !obscureText.value,
                     ),
-                    onTap: () =>
-                        termsAndConditions.value = !termsAndConditions.value,
                   ),
-                ],
-              ),
-              Row(
-                children: [
-                  Checkbox(
-                    value: newsSubscription.value,
-                    onChanged: (_) =>
-                        newsSubscription.value = !newsSubscription.value,
-                  ),
-                  GestureDetector(
-                    child: Text(
-                      context.tr.receiveEmails,
+                  keyboardType: TextInputType.visiblePassword,
+                  validator:
+                      ValidationBuilder().required().minLength(6).build(),
+                ),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: termsAndConditions.value,
+                      onChanged: (_) =>
+                          termsAndConditions.value = !termsAndConditions.value,
                     ),
-                    onTap: () =>
-                        newsSubscription.value = !newsSubscription.value,
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: AppLayouts.defaultPadding,
-              ),
-              Consumer(
-                builder: (context, ref, child) {
-                  final signUpProcess = ref.watch(signInControllerProvider);
-                  return FilledButton(
-                    onPressed: signUpProcess.isLoading
-                        ? null
-                        : () {
-                            ref.read(signInControllerProvider.notifier).signUp(
-                                  emailController.text,
-                                  passwordController.text,
-                                  context,
+                    GestureDetector(
+                      child: Text(
+                        context.tr.termsAndConditionsCheckBoxText,
+                      ),
+                      onTap: () =>
+                          termsAndConditions.value = !termsAndConditions.value,
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: newsSubscription.value,
+                      onChanged: (_) =>
+                          newsSubscription.value = !newsSubscription.value,
+                    ),
+                    GestureDetector(
+                      child: Text(
+                        context.tr.receiveEmails,
+                      ),
+                      onTap: () =>
+                          newsSubscription.value = !newsSubscription.value,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: AppLayouts.defaultPadding,
+                ),
+                Consumer(
+                  builder: (context, ref, child) {
+                    final signUpProcess = ref.watch(signInControllerProvider);
+                    return FilledButton(
+                      onPressed: signUpProcess.isLoading
+                          ? null
+                          : () {
+                              if (termsAndConditions.value == true) {
+                                if (formKey.currentState?.validate() != true) {
+                                  return;
+                                }
+                                ref
+                                    .read(signInControllerProvider.notifier)
+                                    .signUp(
+                                      emailController.text,
+                                      passwordController.text,
+                                      context,
+                                    );
+                              } else {
+                                showDialog<String>(
+                                  context: context,
+                                  builder: (context) => AlertDialog(
+                                    title: Text(context.tr.requiredAction),
+                                    content: Text(
+                                      context.tr.emptyTermsAndConditionsWarning,
+                                    ),
+                                  ),
                                 );
-                          },
-                    child: Text(context.tr.signUp),
-                  );
-                },
-              ),
-              const SizedBox(
-                height: AppLayouts.defaultPadding,
-              ),
-              const GoogleSignInCard(),
-            ],
+                              }
+                            },
+                      child: Text(context.tr.signUp),
+                    );
+                  },
+                ),
+                const SizedBox(
+                  height: AppLayouts.defaultPadding,
+                ),
+                const GoogleSignInCard(),
+              ],
+            ),
           ),
         ),
       ),
