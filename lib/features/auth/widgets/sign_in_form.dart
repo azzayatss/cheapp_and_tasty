aeeb380e18a02fe2e57e12ea15b373eed7b881d3
andrii zayats <andrii.zayats@otakoyi.com>
2023-09-07 13:04:04 +0300

    feat: sign in fields validation

--- lib/features/auth/widgets/sign_in_form.dart
+++ lib/features/auth/widgets/sign_in_form.dart
@@ -5,6 +5,7 @@ import 'package:cheapp_and_tasty/features/auth/widgets/google_sign_in_card.dart'
 import 'package:cheapp_and_tasty/features/auth/widgets/reset_password_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:form_validator/form_validator.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class SignInForm extends HookWidget {
@@ -12,6 +13,7 @@ class SignInForm extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final formKey = useMemoized(GlobalKey<FormState>.new);
     final emailController = useTextEditingController();
     final passwordController = useTextEditingController();
     final obscureText = useState(true);
@@ -19,76 +21,91 @@ class SignInForm extends HookWidget {
       child: Padding(
         padding: const EdgeInsets.all(AppLayouts.defaultPadding),
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: AppLayouts.spacer / 2,
-              ),
-              Text(
-                context.tr.signInEmoji,
-                style: const TextStyle(fontSize: AppLayouts.emojiSize),
-              ),
-              const SizedBox(
-                height: AppLayouts.defaultPadding,
-              ),
-              TextFormField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  hintText: context.tr.emailFormHint,
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: AppLayouts.spacer / 2,
                 ),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              const SizedBox(
-                height: AppLayouts.defaultPadding,
-              ),
-              TextFormField(
-                controller: passwordController,
-                obscureText: obscureText.value,
-                decoration: InputDecoration(
-                  hintText: context.tr.passwordFormHint,
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      obscureText.value
-                          ? Icons.visibility_outlined
-                          : Icons.visibility_off_outlined,
+                Text(
+                  context.tr.signInEmoji,
+                  style: const TextStyle(fontSize: AppLayouts.emojiSize),
+                ),
+                const SizedBox(
+                  height: AppLayouts.defaultPadding,
+                ),
+                TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    hintText: context.tr.emailFormHint,
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: ValidationBuilder()
+                      .required()
+                      .email()
+                      .maxLength(50)
+                      .build(),
+                ),
+                const SizedBox(
+                  height: AppLayouts.defaultPadding,
+                ),
+                TextFormField(
+                  controller: passwordController,
+                  obscureText: obscureText.value,
+                  decoration: InputDecoration(
+                    hintText: context.tr.passwordFormHint,
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        obscureText.value
+                            ? Icons.visibility_outlined
+                            : Icons.visibility_off_outlined,
+                      ),
+                      onPressed: () => obscureText.value = !obscureText.value,
                     ),
-                    onPressed: () => obscureText.value = !obscureText.value,
+                  ),
+                  keyboardType: TextInputType.visiblePassword,
+                  validator:
+                      ValidationBuilder().required().minLength(6).build(),
+                ),
+                const SizedBox(
+                  height: AppLayouts.defaultPadding,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    child: Text(context.tr.forgotPassword),
+                    onPressed: () => _dialogBuilder(context),
                   ),
                 ),
-                keyboardType: TextInputType.visiblePassword,
-              ),
-              const SizedBox(
-                height: AppLayouts.defaultPadding,
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  child: Text(context.tr.forgotPassword),
-                  onPressed: () => _dialogBuilder(context),
+                const SizedBox(
+                  height: AppLayouts.defaultPadding,
                 ),
-              ),
-              const SizedBox(
-                height: AppLayouts.defaultPadding,
-              ),
-              Consumer(
-                builder: (context, ref, child) {
-                  return FilledButton(
-                    child: Text(context.tr.signIn),
-                    onPressed: () => ref
-                        .read(signInControllerProvider.notifier)
-                        .signInWithEmail(
-                          emailController.text,
-                          passwordController.text,
-                          context,
-                        ),
-                  );
-                },
-              ),
-              const SizedBox(
-                height: AppLayouts.defaultPadding * 2,
-              ),
-              const GoogleSignInCard(),
-            ],
+                Consumer(
+                  builder: (context, ref, child) {
+                    return FilledButton(
+                      child: Text(context.tr.signIn),
+                      onPressed: () {
+                        if (formKey.currentState?.validate() ?? false) {
+                          ref
+                              .read(signInControllerProvider.notifier)
+                              .signInWithEmail(
+                                emailController.text,
+                                passwordController.text,
+                                context,
+                              );
+                        }
+                        return;
+                      },
+                    );
+                  },
+                ),
+                const SizedBox(
+                  height: AppLayouts.defaultPadding * 2,
+                ),
+                const GoogleSignInCard(),
+              ],
+            ),
           ),
         ),
       ),
