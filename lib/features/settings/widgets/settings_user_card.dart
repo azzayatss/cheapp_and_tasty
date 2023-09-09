a9957bb24e3bdd547334274071d51b452656c7ef
andrii zayats <andrii.zayats@otakoyi.com>
2023-09-09 17:39:29 +0300

    refactor: real rating deature quick refactor

--- lib/features/settings/widgets/settings_user_card.dart
+++ lib/features/settings/widgets/settings_user_card.dart
@@ -77,59 +77,5 @@ class SettingsUserCard extends HookConsumerWidget {
         child: CircularProgressIndicator.adaptive(),
       ),
     );
-
-    // final userName = useState<String>('');
-    // final userEmail = useState<String>('');
-    // final userPhotoUrl = useState<String>('');
-    // FirebaseAuth.instance.authStateChanges().listen((User? user) {
-    //   if (user != null) {
-    //     userName.value = user.displayName ?? '';
-    //     userEmail.value = user.email ?? '';
-    //     userPhotoUrl.value = user.photoURL ?? '';
-    //   }
-    // });
-
-    // return Card(
-    //   child: Padding(
-    //     padding: const EdgeInsets.all(AppLayouts.defaultPadding),
-    //     child: Row(
-    //       children: [
-    //         Expanded(
-    //           child: CircleAvatar(
-    //             radius: 35,
-    //             child: userPhotoUrl.value.isNotEmpty
-    //                 ? Image.network(
-    //                     userPhotoUrl.value,
-    //                     fit: BoxFit.contain,
-    //                   )
-    //                 : const CircularProgressIndicator.adaptive(),
-    //           ),
-    //         ),
-    //         const SizedBox(
-    //           width: AppLayouts.defaultPadding,
-    //         ),
-    //         Column(
-    //           crossAxisAlignment: CrossAxisAlignment.start,
-    //           children: [
-    //             Text(
-    //               userName.value.isNotEmpty
-    //                   ? userName.value
-    //                   : context.tr.unknownUser,
-    //               style: context.textTheme.headlineSmall,
-    //             ),
-    //             Text(
-    //               userEmail.value.isNotEmpty
-    //                   ? userEmail.value
-    //                   : context.tr.unknownUser,
-    //               style: context.textTheme.bodyMedium,
-    //             ),
-    //           ],
-    //         ),
-    //         const Spacer(),
-    //         const Icon(Icons.arrow_forward_ios),
-    //       ],
-    //     ),
-    //   ),
-    // );
   }
 }
